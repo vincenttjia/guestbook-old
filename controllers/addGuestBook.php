21b0574b3422d5b0e38c10a257d7f5ae02afbd6e
Vincent Tjianattan <vincent.tjianattan@binus.ac.id>
2021-07-25 23:24:35 +0700

    final commit

--- controllers/addGuestBook.php
+++ controllers/addGuestBook.php
@@ -1,29 +1,33 @@
 <?php
     require 'database.php';
 
-    if(isset($_POST['name'])){
+    if(!empty(trim($_POST['name']))){
         $name = $_POST['name'];
     }else{
         echo json_encode(array("error" => "1","message" => "Name can not be empty"));
         die();
     }
 
-    if(isset($_POST['content'])){
+    if(!empty(trim($_POST['content']))){
         $content  = $_POST['content'];
     }else{
         echo json_encode(array("error" => "1","message" => "Message can not be empty"));
         die();
     }
 
+    $affected_rows=0;
+
+
     $q = $connect->prepare("INSERT INTO `guestbook` VALUES ('',?,?,now())");
-    $q->bind_param('ss',$name,$connect);
+    $q->bind_param('ss',$name,$content);
     $q->execute();
     $affected_rows = $q->affected_rows;
     $q->close();
 
+    
     if($affected_rows>0){
         echo json_encode(array("error" => "0"));
     }else{
-        echo json_encode(array("error" => "0","message" => "Undefined Error!"));
+        echo json_encode(array("error" => "1","message" => "Undefined Error!"));
     }
 ?>
--- scripts/main.js
+++ scripts/main.js
@@ -5,13 +5,29 @@ $(function(){
     $submit = $("#submit");
     $name = $("#name");
     $message = $("#message");
+    $alertdiv = $("#alertdiv");
+    $modalAlertDiv = $("#modalAlertDiv");
+    errorAlert = $("#errorAlert")[0].innerHTML;
+    $addModal = $("#addModal");
+    $addButton = $("#addButton");
+    $form = $("#form");
+
 
     function renderTable($data){
         $guestbook_target.append(Mustache.render(guestbook_template,$data));
     }
 
+    function renderAlert($message){
+        $content = {message: $message}
+        $alertdiv.append(Mustache.render(errorAlert,$content))
+    }
+
+    function renderModalAlert($message){
+        $content = {message: $message}
+        $modalAlertDiv.append(Mustache.render(errorAlert,$content))
+    }
+
     function loadtable(){
-        console.log("run");
         $guestbook_target.empty();
         $.ajax({
             type: "GET",
@@ -24,27 +40,53 @@ $(function(){
                         renderTable(data);
                     })
                 }else{
-                    console.log("Undefined Error");
+                    renderAlert("Undefined Error");
                 }
             },
             error: function(err){
-                console.log(err);
+                renderAlert("Please check your internet connection");
             }
         })
     }
 
     function submit(){
-        data : 
         $.ajax({
             type: "POST",
             url: "./controllers/addGuestBook.php",
             data: {
-                name: $name,
-                content: $message
+                name: $name.val(),
+                content: $message.val()
             },
-            
+            dataType: "JSON",
+            success: function(res){
+                console.log(res);
+                if(res.error==0){
+                    loadtable();
+                    $addModal.modal('hide');
+                    $name.val("");
+                    $message.val("");
+                    $submit.prop('disabled', false);
+                }else{
+                    $submit.prop('disabled', false);
+                    renderModalAlert(res.message);
+                }
+            },
+            error: function(err){
+                console.log(err);
+                $submit.prop('disabled', false);
+                renderModalAlert("Please check your internet connection");
+            }
         })
     }
+
+    $addModal.on('shown.bs.modal', function () {
+        $name.focus();
+    }) 
+
+    $addButton.on("click", function(){
+        $modalAlertDiv.empty();
+        
+    })
 
     loadtable();
 
@@ -52,7 +94,10 @@ $(function(){
         loadtable();
     });
 
-    $submit.on("click",function(){
-
+    $form.on("submit", function(e){
+        $modalAlertDiv.empty();
+        e.preventDefault();
+        $submit.prop('disabled', true);
+        submit();
     })
 })
