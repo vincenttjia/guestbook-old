0bb524f7ccd0a8184bcf8da1b8e7ea8761110ae4
Vincent Tjianattan <vincent.tjianattan@binus.ac.id>
2021-07-26 15:18:40 +0700

    add memcache support for secret manager

--- controllers/database.php
+++ controllers/database.php
@@ -1,6 +1,17 @@
 <?php
     require 'loadCred.php';
 
+    $mem = new Memcached();
+    $mem->addServer("127.0.0.1", 11211);
+
+    $result = $mem->get("cred");
+
+    if($result){
+        $config = json_decode($result,true);
+    }else{
+        $config = json_decode(get_credentials(),true);
+    }
+
     $connect = new mysqli($config['DB_HOST'],$config['DB_USERNAME'],$config['DB_PASSWORD'],$config['DB_DATABASE']);
 
 ?>
--- controllers/loadCred.php
+++ controllers/loadCred.php
@@ -22,6 +22,9 @@ require './../vendor/autoload.php';
 use Aws\SecretsManager\SecretsManagerClient; 
 use Aws\Exception\AwsException;
 
+
+
+
 /**
  * List the SecretId for each Secret in the account for the selected region.
  *
@@ -30,53 +33,64 @@ use Aws\Exception\AwsException;
  */
 
 //Create a Secrets Manager Client 
-$client = new SecretsManagerClient([
-    'version' => '2017-10-17',
-    'region' => 'us-east-1'
-]);
 
-$secretName = 'guestbook';
+function get_credentials(){
 
-try {
-    $result = $client->getSecretValue([
-        'SecretId' => $secretName,
+    $mem = new Memcached();
+    $mem->addServer("127.0.0.1", 11211);
+
+    $client = new SecretsManagerClient([
+        'version' => '2017-10-17',
+        'region' => 'us-east-1'
     ]);
+    
+    $secretName = 'guestbook';
+    
+    try {
+        $result = $client->getSecretValue([
+            'SecretId' => $secretName,
+        ]);
+    
+    } catch (AwsException $e) {
+        $error = $e->getAwsErrorCode();
+        if ($error == 'DecryptionFailureException') {
+            // Secrets Manager can't decrypt the protected secret text using the provided AWS KMS key.
+            // Handle the exception here, and/or rethrow as needed.
+            throw $e;
+        }
+        if ($error == 'InternalServiceErrorException') {
+            // An error occurred on the server side.
+            // Handle the exception here, and/or rethrow as needed.
+            throw $e;
+        }
+        if ($error == 'InvalidParameterException') {
+            // You provided an invalid value for a parameter.
+            // Handle the exception here, and/or rethrow as needed.
+            throw $e;
+        }
+        if ($error == 'InvalidRequestException') {
+            // You provided a parameter value that is not valid for the current state of the resource.
+            // Handle the exception here, and/or rethrow as needed.
+            throw $e;
+        }
+        if ($error == 'ResourceNotFoundException') {
+            // We can't find the resource that you asked for.
+            // Handle the exception here, and/or rethrow as needed.
+            throw $e;
+        }
+    }
+    // Decrypts secret using the associated KMS CMK.
+    // Depending on whether the secret is a string or binary, one of these fields will be populated.
+    if (isset($result['SecretString'])) {
+        $secret = $result['SecretString'];
+    } else {
+        $secret = base64_decode($result['SecretBinary']);
+    }   
 
-} catch (AwsException $e) {
-    $error = $e->getAwsErrorCode();
-    if ($error == 'DecryptionFailureException') {
-        // Secrets Manager can't decrypt the protected secret text using the provided AWS KMS key.
-        // Handle the exception here, and/or rethrow as needed.
-        throw $e;
-    }
-    if ($error == 'InternalServiceErrorException') {
-        // An error occurred on the server side.
-        // Handle the exception here, and/or rethrow as needed.
-        throw $e;
-    }
-    if ($error == 'InvalidParameterException') {
-        // You provided an invalid value for a parameter.
-        // Handle the exception here, and/or rethrow as needed.
-        throw $e;
-    }
-    if ($error == 'InvalidRequestException') {
-        // You provided a parameter value that is not valid for the current state of the resource.
-        // Handle the exception here, and/or rethrow as needed.
-        throw $e;
-    }
-    if ($error == 'ResourceNotFoundException') {
-        // We can't find the resource that you asked for.
-        // Handle the exception here, and/or rethrow as needed.
-        throw $e;
-    }
-}
-// Decrypts secret using the associated KMS CMK.
-// Depending on whether the secret is a string or binary, one of these fields will be populated.
-if (isset($result['SecretString'])) {
-    $secret = $result['SecretString'];
-} else {
-    $secret = base64_decode($result['SecretBinary']);
+        $mem->set("cred", $secret);
+        
+        return $secret;
+
 }
 
-    $config = json_decode($secret,true);
 ?>
